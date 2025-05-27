e5b372edae29516f8712b53da8f74fd498388682
ansteishass <anasteisha_m386@mail.ru@whatever.com>
2025-05-27 14:54:42 +0000

    Add /metrics endpoint (version 2.0)

--- app.py
+++ app.py
@@ -2,10 +2,17 @@
   import time
 
   app = Flask(__name__)
+  time_requests = 0
 
   @app.route('/time')
   def get_time():
+      global time_requests
+      time_requests += 1
       return jsonify({"time": int(time.time())})
+
+  @app.route('/metrics')
+  def get_metrics():
+      return jsonify({"count": time_requests})
 
   if __name__ == '__main__':
       app.run(host='0.0.0.0', port=5000)
