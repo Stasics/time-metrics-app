abcf11e1fff634a0c558ce4a7537d11ad2c2bf0e
Anasteisha <anasteisha.s333@yandex.ru@whatever.com>
2025-05-30 09:16:09 +0000

    Add test for /metrics

---
+++ test_app.py
@@ -0,0 +1,9 @@
+def test_metrics_route(client):
+    # Сначала делаем запрос к /time, чтобы увеличить счётчик
+    client.get('/time')
+    response = client.get('/metrics')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert 'count' in data
+    assert data['count'] == 1
+  
