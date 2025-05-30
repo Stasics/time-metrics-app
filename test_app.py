e0c1f3abc40a55fd3a9427c130aa8435ceaf0e5f
Anasteisha <anasteisha.s333@yandex.ru@whatever.com>
2025-05-30 09:25:43 +0000

    Add test for /metrics

--- test_app.py
+++ test_app.py
@@ -1,4 +1,17 @@
-#2
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_time_route(client):
+    response = client.get('/time')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert 'time' in data
+    assert data['time'] != 0
 def test_metrics_route(client):
     # Сначала делаем запрос к /time, чтобы увеличить счётчик
     client.get('/time')
@@ -8,3 +21,4 @@ def test_metrics_route(client):
     assert 'count' in data
     assert data['count'] == 1
   
+
